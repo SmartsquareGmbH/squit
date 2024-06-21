5794f5770d652b524f8b94262d4d0f1356f89050
Thomas Sievert <sievert@smartsquare.de>
2024-06-21 11:40:30 +0200

    add option to configure pre- and post-testtasks per test

---
+++ src/main/kotlin/de/smartsquare/squit/task/SquitPostTestTask.kt
@@ -0,0 +1,5 @@
+package de.smartsquare.squit.task
+
+enum class SquitPostTestTask {
+    DATABASE_SCRIPTS, POST_RUNNERS, POST_RUNNER_SCRIPTS
+}
---
+++ src/test/resources/test-project-task-config/src/squit/project/pre_run.groovy
@@ -0,0 +1,5 @@
+import java.time.Instant
+Thread.sleep(1)
+def file = new File(config.getString("rootDir") + "/build/pre_run.txt")
+file.text = Instant.now().toEpochMilli().toString()
+Thread.sleep(1)
